943db54f537bd55b79abae3a422fa11d4a995ebc
Chao Liang <52976469+Chao0813@users.noreply.github.com>
2019-08-07 13:54:43 -0400

    Create app.py

---
+++ app.py
@@ -0,0 +1,26 @@
+from flask import Flask, render_template, request
+import urllib.request
+import json
+
+
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def home():
+    url = 'https://api.nasa.gov/planetary/apod?api_key='
+    key = 'fOrNYkZMYt8KEwieCkuEcYJi3uMYQvQpvDDFjEmv'
+    urlobj = urllib.request.urlopen(url + key)
+    read = urlobj.read()
+    decode = json.loads(read.decode('utf-8'))
+    image = decode['url'
+    return render_template('layout1.html', image=image)
+@app.route('/key', methods=['POST'])
+def key():
+    key = request.form['apikey']
+    image = buildURL(url, key)
+    return render_template('layout1.html', image=image)
+
+if __name__ == '__main__':
+    app.run(debug=True)
